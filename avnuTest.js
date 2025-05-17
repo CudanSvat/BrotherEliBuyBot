6d8ef0af95091c8d0d610ec099c91155a13b9d8c
CudanSvat <CudanSvat@users.noreply.github.com>
2025-05-17 22:13:57 +0200

    Update avnuTest.js

--- avnuTest.js
+++ avnuTest.js
@@ -4,7 +4,7 @@ const TOKEN_ID = '0x02ab526354a39e7f5d272f327fa94e757df3688188d4a92c6dc3623ab798
 
 async function testAvnuAPI() {
   try {
-    const url = `https://api.avnu.fi/api/v1/spot/tokens/${TOKEN_ID}/trades?limit=5&sort=desc&filter=buy`;
+    const url = `https://starknet.api.avnu.fi/api/v1/spot/tokens/${TOKEN_ID}/trades?limit=5&sort=desc&filter=buy`;
     const response = await axios.get(url);
     console.log('Avnu API response:', response.data);
   } catch (error) {
